fa91064ffbb615c1cae47dc3a4734a31b8816a1c
henry-jacq <henryeditz267@gmail.com>
2024-11-14 20:57:30 +0000

    feat: update traffic display and data processing for improved accuracy and clarity

--- server/modules/consumer.py
+++ server/modules/consumer.py
@@ -21,10 +21,7 @@ class KafkaPacketConsumer:
                 *topics,
                 bootstrap_servers=self.kafka_config['bootstrap_servers'],
                 auto_offset_reset=self.kafka_config['auto_offset_reset'],
-                value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-                group_id=self.kafka_config['group_id'],
-                session_timeout_ms=30000,
-                max_poll_interval_ms=600000
+                value_deserializer=lambda x: json.loads(x.decode('utf-8'))
             )
             logging.info(f"Kafka consumer initialized for topics: {', '.join(topics)}")
             return consumer
@@ -37,40 +34,33 @@ class KafkaPacketConsumer:
         protocol_no = protocol_mapping.get(message_value['protocol'], 6)
 
         parameters = {
-            'Source IP': self.model.ip_to_numeric(message_value.get('src_ip', 0)),
-            'Source Port': int(message_value.get('src_port', 0)),
-            'Destination Port': int(message_value.get('dst_port', 0)),
-            'Protocol': protocol_no,
-            'Flow Duration': int(message_value.get('flow_duration', 0)),
-            'Total Fwd Packets': int(message_value.get('total_fwd_packets', 0)),
-            'Total Backward Packets': int(message_value.get('total_bwd_packets', 0)),
-            'Total Length of Fwd Packets': float(message_value.get('total_len_of_fwd_packets', 0.0)),
-            'Fwd Packet Length Max': float(message_value.get('fwd_packet_length_max', 0.0)),
-            'Fwd Packet Length Std': float(message_value.get('fwd_packet_length_std', 0.0)),
-            'Bwd Packet Length Max': float(message_value.get('bwd_packet_length_max', 0.0)),
-            'Bwd Packet Length Min': float(message_value.get('bwd_packet_length_min', 0.0)),
-            'Bwd Packet Length Mean': float(message_value.get('bwd_packet_length_mean', 0.0)),
-            'Flow Packets/s': float(message_value.get('flow_packets_per_sec', 0.0)),
-            'Flow IAT Min': float(message_value.get('flow_iat_min', 0.0)),
-            'Bwd IAT Total': float(message_value.get('bwd_iat_total', 0.0)),
-            'Bwd IAT Min': float(message_value.get('bwd_iat_min', 0.0)),
-            'Fwd PSH Flags': int(message_value.get('fwd_psh_flags', 0)),
-            'Fwd Header Length': int(message_value.get('fwd_header_length', 0)),
-            'Bwd Header Length': int(message_value.get('bwd_header_length', 0)),
-            'Bwd Packets/s': float(message_value.get('bwd_packets_per_sec', 0.0)),
-            'Packet Length Variance': float(message_value.get('packet_length_variance', 0.0)),
-            'SYN Flag Count': int(message_value.get('syn_flag_count', 0)),
-            'URG Flag Count': int(message_value.get('urg_flag_count', 0)),
-            'CWE Flag Count': int(message_value.get('cwe_flag_count', 0)),
-            'Down/Up Ratio': float(message_value.get('down_up_ratio', 0.0)),
-            'Init_Win_bytes_forward': int(message_value.get('init_win_bytes_forward', 0)),
-            'Init_Win_bytes_backward': int(message_value.get('init_win_bytes_backward', 0)),
+            'src_ip': self.model.ip_to_numeric(message_value.get('src_ip', 0)),
+            'src_port': int(message_value.get('src_port', 0)),
+            'dst_port': int(message_value.get('dst_port', 0)),
+            'protocol': protocol_no,
+            'flow_duration': float(message_value.get('flow_duration', 0.0)),
+            'total_fwd_packets': int(message_value.get('total_fwd_packets', 0)),
+            'total_bwd_packets': int(message_value.get('total_bwd_packets', 0)),
+            'total_len_of_fwd_packets': int(message_value.get('total_len_of_fwd_packets', 0)),
+            'fwd_packet_length_max': int(message_value.get('fwd_packet_length_max', 0)),
+            'fwd_packet_length_std': float(message_value.get('fwd_packet_length_std', 0.0)),
+            'bwd_packet_length_max': int(message_value.get('bwd_packet_length_max', 0)),
+            'flow_packets_per_sec': float(message_value.get('flow_packets_per_sec', 0.0)),
+            'fwd_header_length': int(message_value.get('fwd_header_length', 0)),
+            'packet_length_variance': float(message_value.get('packet_length_variance', 0.0)),
+            'syn_flag_count': int(message_value.get('syn_flag_count', 0)),
+            'urg_flag_count': int(message_value.get('urg_flag_count', 0)),
+            'init_win_bytes_forward': int(message_value.get('init_win_bytes_forward', 0)),
             'act_data_pkt_fwd': int(message_value.get('act_data_pkt_fwd', 0)),
             'min_seg_size_forward': int(message_value.get('min_seg_size_forward', 0)),
-            'Active Mean': float(message_value.get('active_mean', 0.0)),
-            'Active Std': float(message_value.get('active_std', 0.0)),
-            'Inbound': int(message_value.get('inbound', 0))
+            'active_mean': float(message_value.get('active_mean', 0.0)),
+            'inbound': int(message_value.get('inbound', 0)),
+            'flow_iat_min': float(message_value.get('flow_iat_min', 0.0)),
+            'bwd_iat_total': int(message_value.get('bwd_iat_total', 0)),
+            'fwd_psh_flags': int(message_value.get('fwd_psh_flags', 0)),
+            'down_up_ratio': float(message_value.get('down_up_ratio', 0.0)),
         }
+
         return self.model.preprocess_data([parameters])
 
     def predict(self, preprocessed_data):
@@ -104,10 +94,13 @@ class KafkaPacketConsumer:
         if consumer:
             try:
                 for message in consumer:
+                    # print(message.value)
                     self.packet_count += 1
 
-                    preprocessed_data = self.preprocess_data(message.value)
-                    prediction_result = self.predict(preprocessed_data)
+                    # preprocessed_data = self.preprocess_data(message.value)
+                    # prediction_result = self.predict(preprocessed_data)
+
+                    # print(prediction_result)
 
                     # Extract protocol from the message
                     protocol = message.value.get('protocol', 'Unknown')
@@ -118,13 +111,16 @@ class KafkaPacketConsumer:
                     # attacks on ports
                     attack_port = message.value.get('dst_port', 'Unknown')
 
+                    # Add timestamp to the message
+                    message.value['timestamp'] = message.timestamp
+                    
                     # Emit the original message and the prediction
                     self.socketio.emit(message.topic, message.value)
-                    self.socketio.emit("prediction", prediction_result)
+                    self.socketio.emit("prediction", {"attack_type": "BENIGN"})
 
 
-                    # Calculate packets/sec every 2 seconds
-                    if time.time() - self.start_time >= 2:
+                    # Calculate packets/sec every second
+                    if time.time() - self.start_time >= 1:
                         # Emit the protocol via socket
                         self.socketio.emit("protocol", {"protocol": protocol})
                         self.socketio.emit("attack_ip", {"attack_ip": attack_ip})
